88f42f4c2bc4f6dc07ec915e181fc412bd4c7d46
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-04-25 08:15:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -82,3 +82,31 @@ team.addEventListener("click", () => {
 events.addEventListener("click", () => {
     window.location.href = "events.html";
 });
+
+// Image Gallery
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
